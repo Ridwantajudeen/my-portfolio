6dfa314fbf31f3066f0410717b2b398b6c1400bd
Ridwan Tajudeen <108151117+Ridwantajudeen@users.noreply.github.com>
2025-07-24 01:35:01 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Hero from "./components/hero";
+import Hero from "./components/Hero";
 import About from "./components/About";
 import Projects from "./components/Projects";
 import Skills from "./components/Skills";
