fc74f7a8a2e6a146a1b4dc500222d6640ec99a88
ridwantajudeen <ridwantajudeen8@gmail.com>
2025-07-24 02:31:31 +0100

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Hero from "./components/hero";
+import { Helmet } from "react-helmet";
+import Hero from "./components/Hero";
 import About from "./components/About";
 import Projects from "./components/Projects";
 import Skills from "./components/Skills";
@@ -10,6 +11,7 @@ function App() {
   return (
     <>
       <Helmet>
+        
         <title>Ridwan Tajudeen | Web Developer & WordPress Expert</title>
         <meta
           name="description"
@@ -20,14 +22,33 @@ function App() {
           content="Ridwan Tajudeen, web developer, WordPress developer, React, Web3 developer, blockchain, Nigeria, portfolio"
         />
         <meta name="robots" content="index, follow" />
+
+        
+        <meta property="og:url" content="https://ridwantajudeen.vercel.app/" />
+        <meta property="og:type" content="website" />
         <meta property="og:title" content="Ridwan Tajudeen | Web Developer & WordPress Expert" />
         <meta
           property="og:description"
           content="I help clients build fast websites, custom WordPress solutions, and Web3 blockchain apps."
         />
-        <meta property="og:url" content="https://ridwantajudeen.vercel.app" />
-        <meta property="og:type" content="website" />
-        <meta property="og:image" content="https://ridwantajudeen.vercel.app/preview.png" /> 
+        <meta
+          property="og:image"
+          content="https://opengraph.b-cdn.net/production/images/e098907e-60d1-4715-bb76-5b592091ddfc.png?token=HDzKPdOQRjhz462yuLQK2ydmCu3Zw4wfvm2cDBad5us&height=611&width=1200&expires=33289320383"
+        />
+
+        
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta property="twitter:domain" content="ridwantajudeen.vercel.app" />
+        <meta property="twitter:url" content="https://ridwantajudeen.vercel.app/" />
+        <meta name="twitter:title" content="Ridwan Tajudeen | Web Developer & WordPress Expert" />
+        <meta
+          name="twitter:description"
+          content="I help clients build fast websites, custom WordPress solutions, and Web3 blockchain apps."
+        />
+        <meta
+          name="twitter:image"
+          content="https://opengraph.b-cdn.net/production/images/e098907e-60d1-4715-bb76-5b592091ddfc.png?token=HDzKPdOQRjhz462yuLQK2ydmCu3Zw4wfvm2cDBad5us&height=611&width=1200&expires=33289320383"
+        />
       </Helmet>
 
       <Router>
@@ -36,8 +57,6 @@ function App() {
           <Route path="/" element={<Hero />} />
           <Route path="/about" element={<About />} />
         </Routes>
-
-        
         <Projects />
         <Skills />
         <Contact />
